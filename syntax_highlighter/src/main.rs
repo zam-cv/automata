393b50c9f8fafa31d0dc44014e320c843dbb7d1c
zam-cv <a01799283@tec.mx>
2024-04-03 13:57:23 -0600

    add: lexer

--- syntax_highlighter/src/main.rs
+++ syntax_highlighter/src/main.rs
@@ -30,122 +30,195 @@ enum Expression<'a> {
     InternalRule(&'a str),
 }
 
-fn get_graph(grammar: &str) -> anyhow::Result<HashMap<&str, Vec<Vec<Expression>>>> {
-    let mut graph = HashMap::new();
+struct Analyzer<'a> {
+    pub initial_rule: &'a str,
+    pub grammar: HashMap<&'a str, Vec<Vec<Expression<'a>>>>,
+}
 
-    for (i, line) in grammar.lines().enumerate() {
-        if line.is_empty() {
-            continue;
-        }
+impl<'a> Analyzer<'a> {
+    fn new(grammar: &'a str, initial_rule: &'a str) -> anyhow::Result<Self> {
+        let mut map = HashMap::new();
 
-        let mut parts = line.splitn(2, " = ");
+        for (i, line) in grammar.lines().enumerate() {
+            if line.is_empty() {
+                continue;
+            }
 
-        if let (Some(key), Some(value)) = (parts.next(), parts.next()) {
-            let mut options = Vec::new();
+            let mut parts = line.splitn(2, " = ");
 
-            for option in value.split(" | ") {
-                let mut components = Vec::new();
+            if let (Some(key), Some(value)) = (parts.next(), parts.next()) {
+                let mut options = Vec::new();
 
-                for component in option.split(" ~ ") {
-                    let component = component.trim();
+                for option in value.split(" | ") {
+                    let mut expressions = Vec::new();
 
-                    if component.len() > 0 {
-                        if component.starts_with("\"") {
-                            components.push(Expression::String(&component[1..component.len() - 1]));
-                            continue;
-                        } else if INTERNAL_RULE.contains_key(component) {
-                            components.push(Expression::InternalRule(component));
-                            continue;
-                        } else {
-                            if component.ends_with("_keyword") {
-                                components.push(Expression::Keyword(component));
+                    for component in option.split(" ~ ") {
+                        let component = component.trim();
+
+                        if component.len() > 0 {
+                            if component.starts_with("\"") {
+                                expressions
+                                    .push(Expression::String(&component[1..component.len() - 1]));
+                                continue;
+                            } else if INTERNAL_RULE.contains_key(component) {
+                                expressions.push(Expression::InternalRule(component));
+                                continue;
                             } else {
-                                components.push(Expression::Rule(component));
+                                if component.ends_with("_keyword") {
+                                    expressions.push(Expression::Keyword(component));
+                                } else {
+                                    expressions.push(Expression::Rule(component));
+                                }
+                                continue;
                             }
-                            continue;
                         }
+
+                        return Err(anyhow::anyhow!("Invalid grammar at line {}", i + 1));
                     }
 
-                    return Err(anyhow::anyhow!("Invalid grammar at line {}", i + 1));
+                    options.push(expressions);
                 }
 
-                options.push(components);
+                map.insert(key, options);
+            } else {
+                return Err(anyhow::anyhow!("Invalid grammar at line {}", i + 1));
             }
-
-            graph.insert(key, options);
-        } else {
-            return Err(anyhow::anyhow!("Invalid grammar at line {}", i + 1));
         }
+
+        Ok(Self {
+            grammar: map,
+            initial_rule,
+        })
     }
 
-    Ok(graph)
-}
+    fn validate(&self) -> anyhow::Result<()> {
+        let mut visited = HashSet::new();
 
-fn validate_graph(
-    initial_rule: &str,
-    graph: &HashMap<&str, Vec<Vec<Expression>>>,
-) -> anyhow::Result<()> {
-    let mut visited = HashSet::new();
+        for (_, options) in self.grammar.iter() {
+            for option in options {
+                for expression in option {
+                    match expression {
+                        Expression::Rule(rule) | Expression::Keyword(rule) => {
+                            visited.insert(rule);
 
-    for (_, options) in graph.iter() {
-        for option in options {
-            for expression in option {
-                match expression {
-                    Expression::Rule(rule) | Expression::Keyword(rule) => {
-                        visited.insert(rule);
+                            if !self.grammar.contains_key(rule) {
+                                return Err(anyhow::anyhow!("Invalid rule: {}", rule));
+                            }
+                        }
+                        Expression::InternalRule(rule) => {
+                            if !INTERNAL_RULE.contains_key(rule) {
+                                return Err(anyhow::anyhow!("Invalid internal rule: {}", rule));
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
 
-                        if !graph.contains_key(rule) {
-                            return Err(anyhow::anyhow!("Invalid rule: {}", rule));
+        for rule in self.grammar.keys() {
+            if !visited.contains(rule) && rule != &self.initial_rule {
+                return Err(anyhow::anyhow!("Unused rule: {}", rule));
+            }
+
+            if rule.ends_with("_keyword")
+                && (self.grammar[rule].len() != 1 || self.grammar[rule][0].len() != 1)
+            {
+                return Err(anyhow::anyhow!("Invalid keyword rule: {}", rule));
+            }
+        }
+
+        Ok(())
+    }
+
+    fn lexer(&self, rule: &'a str, start: &mut usize, input: &'a str) -> Vec<Expression<'a>> {
+        let mut tokens = Vec::new();
+
+        if let Some(options) = self.grammar.get(rule) {
+            'options: for option in options {
+                let mut local_start = *start;
+                let mut temp_tokens = Vec::new();
+
+                for expression in option {
+                    match expression {
+                        Expression::String(string) => {
+                            if input[local_start..].starts_with(string) {
+                                temp_tokens.push(Expression::String(
+                                    &input[local_start..local_start + string.len()],
+                                ));
+                                local_start += string.len();
+                            } else {
+                                continue 'options;
+                            }
+                        }
+                        Expression::Rule(r) => {
+                            let mut temp = self.lexer(r, &mut local_start, input);
+
+                            if local_start == *start {
+                                continue 'options;
+                            }
+
+                            temp_tokens.append(&mut temp);
+                        }
+                        Expression::InternalRule(rule) => {
+                            if rule == &"EMPTY" {
+                                continue;
+                            }
+
+                            let mut end = local_start;
+
+                            while end < input.len() {
+                                if INTERNAL_RULE[rule](&input[end..end + 1]) {
+                                    end += 1;
+                                } else {
+                                    break;
+                                }
+                            }
+
+                            if local_start == end {
+                                continue 'options;
+                            }
+
+                            temp_tokens.push(Expression::String(&input[local_start..end]));
+                            local_start = end;
+                        }
+                        Expression::Keyword(rule) => {
+                            if let Expression::String(string) = &self.grammar[rule][0][0] {
+                                if input[local_start..].starts_with(string) {
+                                    temp_tokens.push(Expression::Keyword(
+                                        &input[local_start..local_start + string.len()],
+                                    ));
+                                    local_start += string.len();
+                                } else {
+                                    continue 'options;
+                                }
+                            }
                         }
                     }
-                    _ => {}
                 }
+
+                *start = local_start;
+                tokens.append(&mut temp_tokens);
+                return tokens;
             }
         }
-    }
 
-    for rule in graph.keys() {
-        if !visited.contains(rule) && rule != &initial_rule {
-            return Err(anyhow::anyhow!("Unused rule: {}", rule));
-        }
-    }
-
-    Ok(())
-}
-
-fn lexer<'a>(
-    rule: &str,
-    start: usize,
-    input: &'a str,
-    tokens: &mut Vec<Expression<'a>>,
-    graph: &HashMap<&str, Vec<Vec<Expression>>>,
-) {
-    if let Some(options) = graph.get(rule) {
-        for option in options {
-            println!("{:?}", option);
-        }
+        return tokens;
     }
 }
 
-fn analyzer(initial_rule: &str, grammar: &str, input: &str) -> anyhow::Result<()> {
-    let graph = get_graph(grammar)?;
-    validate_graph(initial_rule, &graph)?;
-
-    for (key, value) in &graph {
-        println!("{}: {:?}", key, value);
-    }
-
-    // let mut tokens = Vec::new();
-    // lexer(initial_rule, 0, input, &mut tokens, &graph);
-    // println!("{:?}", tokens);
-
-    Ok(())
-}
-
-fn main() {
+fn main() -> anyhow::Result<()> {
     let input = fs::read_to_string("src/test.txt").unwrap();
 
-    if let Err(e) = analyzer("program", GRAMMAR, &input) {
-        eprintln!("{}", e);
-    }
+    let analyzer = Analyzer::new(GRAMMAR, "program")?;
+    analyzer.validate()?;
+
+    // for (key, value) in analyzer.grammar.iter() {
+    //     println!("{}: {:?}", key, value);
+    // }
+
+    let tokens = analyzer.lexer("program", &mut 0, &input);
+    println!("{:?}", tokens);
+
+    Ok(())
 }

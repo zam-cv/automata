5a5a5276bf6ddc2b9c9f751b66bab0d727edc29d
zam-cv <a01799283@tec.mx>
2024-04-21 22:15:32 -0600

    merge

--- dfa/main.py
+++ dfa/main.py
@@ -1,42 +1,35 @@
+# Joel Vargas Reynoso A01752464
+# Carlos Aberto Zamudio Velazquez A01799283
+
 import ast
 import os
-
-# ['a', 'b'] -> Símbolos del alfabeto
-# 5          -> Número de estados
-# [2, 4]     -> Lista de estados de aceptación
-
-#     0: 1:
-# 0: [1, 3]
-# 1: [1, 2]
-# 2: [1, 2]
-# 3: [4, 3]
-# 4: [4, 3]
 
 def dfa_test(input_filename, test_filename):
     symbols: list[str]
     aceptation_states: set[int]
     graph: list[list[int]] = []
 
+    # Pre-procesamiento del archivo
     with open(input_filename, 'r') as file:
-        symbols = ast.literal_eval(file.readline())
-        file.readline()  # Ignorar el número de estados
-        aceptation_states = set(ast.literal_eval(file.readline()))
-
-        for line in file:
-            graph.append(ast.literal_eval(line))
+        # "ast.literal_eval" para una evaluación segura de los datos
+        symbols = ast.literal_eval(file.readline().strip())
+        file.readline()  # Ignora la línea del número de estados
+        aceptation_states = set(ast.literal_eval(file.readline().strip()))
+        graph = [ast.literal_eval(line.strip()) for line in file]
 
     response: str = ''
 
     with open(test_filename, 'r') as file:
         for line in file:
+            line = line.strip() # Elimina los espacios en blanco y saltos de línea
             current_state = 0
-            
+
             for symbol in line:
               transitions = graph[current_state]
 
               for index, state in enumerate(transitions):
                 transition_symbol = symbols[index]
-                
+
                 if transition_symbol == symbol:
                   current_state = state
                   break
@@ -49,7 +42,11 @@ def dfa_test(input_filename, test_filename):
     base, extension = os.path.splitext(test_filename)
     output_file_name = f"{base}-output{extension}"
     with open(output_file_name, 'w') as file:
-        file.write(response)
+      # Escribe todos los resultados de una vez para
+      # minimizar la escritura en disco
+      file.writelines(response)
 
+# Itera sobre un rango fijo de archivos de prueba, a
+# sumiendo que existen y están correctamente nombrados
 for i in range(1, 5):
     dfa_test(f'DFA-pruebas/dfa-0{i}.txt', f'DFA-pruebas/test-0{i}.txt')
